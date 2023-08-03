8636fb8ddc3d31ef1221f7f9b5c58d107b5c11ed
Dhyey Bhattasana <69715862+DhyeyB@users.noreply.github.com>
2023-08-03 10:50:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 module "next-js" {
   source = "./modules/next-js"
-  #   environment             = var.environment
   project_name            = var.project_name
   github_repository       = var.github_repository
   github_token_for_webapp = var.github_token_for_webapp
